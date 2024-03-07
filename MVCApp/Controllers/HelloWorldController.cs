3e1cd7f526c752749cc20380739b4acf893e73e2
Joel Alejandro Nachez Torres <joel.alejandro.nt@hotmail.com>
2024-03-07 16:54:15 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1) {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        //Get: /HelloWorld/
+        //public string Index() 
+        //{
+        //    return "This is default action...";
+        //}
+
+        //Get: /HelloWorld/Welcome/
+        //Requieres using System.Text.Encodings.Web;
+        //Get: /HelloWorld/Welcome?name=Alejandro&numtimes=5/
+        //public string Welcome(string name, int numTimes = 1)
+        //{ 
+        //    //return "This is the welcome action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+    }
+}
---
+++ MVCApp/Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCApp.Data;
+using System;
+using System.Linq;
+
+namespace MVCApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCAppContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MVCAppContext>>()))
+            {
+                //Look for any Movies
+                if (context.Movie.Any())
+                {
+                    return; //Db has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Iron Man",
+                        ReleaseDate = DateTime.Parse("2006-05-04"),
+                        Genre = "Action",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "The king lion",
+                        ReleaseDate = DateTime.Parse("1992-05-04"),
+                        Genre = "Animete",
+                        Price = 1.88M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Toy Story",
+                        ReleaseDate = DateTime.Parse("1999-01-01"),
+                        Genre = "Animete",
+                        Price = 2.75M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-04-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "R"
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
